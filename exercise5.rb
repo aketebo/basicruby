7179ab9fb61f575612257aeba50579813440286a
amanketebo <amanuelketebo@yahoo.com>
2017-08-03 21:42:57 -0700

    Add exercise location comment

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,5 @@
+# Exercises from https://launchschool.com/books/ruby/read/intro_exercises
+
 # Use the each method of Array to iterate over 
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], and print out
 # each value.
